47130f77d5cb99e846ec13519037146c1bf436f1
SathishInfyL <sathish.infy.ltd@gmail.com>
2023-04-25 17:43:03 +0530

    First commit

---
+++ src/test/java/packag/Sample.java
@@ -0,0 +1,31 @@
+package packag;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+
+		/*
+		 * opt.addArguments("--headless"); opt.addArguments("--no-sandbox");
+		 * opt.addArguments("--disable-dev-shm-usage");
+		 * System.setProperty("webdriver.http.factory", "jdk-http-client");
+		 */
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--remote-allow-origins=*");
+		ChromeDriver driver = new ChromeDriver(chromeOptions);
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.google.com");
+		Assert.assertTrue(driver.getTitle().contains("Google"), "Title not match");
+		driver.quit();
+
+	}
+
+}
